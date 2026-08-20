68b9b58145084a5508a97da45611fbf2f4f17871
Sabrina Campos De Araujo <sabrinacvaraujo@gmail.com>
2026-08-20 11:13:59 -0300

    Me de um fork

---
+++ teste.py
@@ -0,0 +1,32 @@
+alunos = []
+
+def adicionar_aluno():
+    nome = input("Digite o nome do aluno: ")
+    alunos.append(nome)
+    print("Aluno cadastrado com sucesso!")
+
+def listar_alunos():
+    print("\n--- ALUNOS ---")
+
+    for aluno in alunos:
+        print(aluno)
+
+while True:
+
+    print("\n1 - Adicionar aluno")
+    print("2 - Listar alunos")
+    print("3 - Sair")
+
+    opcao = input("Escolha: ")
+
+    if opcao == "1":
+        adicionar_aluno()
+
+    elif opcao == "2":
+        listar_alunos()
+
+    elif opcao == "3":
+        break
+
+    else:
+        print("Opção inválida!")
