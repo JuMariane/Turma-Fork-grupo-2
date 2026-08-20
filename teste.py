16817187b3dacaacb133237f49ffd02dc8ee481f
Julia Mariane Dos Santos <0000110074650xsp@aluno.educacao.sp.gov.br>
2026-08-20 18:47:23 -0300

    Criar Buscar

--- teste.py
+++ teste.py
@@ -6,16 +6,29 @@ def adicionar_aluno():
     print("Aluno cadastrado com sucesso!")
 
 def listar_alunos():
-    print("\n--- ALUNOS ---")
-
+    print("\n--- ALUNOS Inserindo---")
+    print("Modificacao")
+    if not alunos:
+        print("Nenhum aluno cadastrado tesr.")
     for aluno in alunos:
         print(aluno)
 
-while True:
+def buscar_aluno():
+    nome_busca = input("Digite o nome do aluno que deseja buscar: ")
+    encontrados = [aluno for aluno in alunos if nome_busca.lower() in aluno.lower()]
+    
+    print("\n--- RESULTADO DA BUSCA ---")
+    if encontrados:
+        for aluno in encontrados:
+            print(f"Aluno encontrado: {aluno}")
+    else:
+        print("Nenhum aluno encontrado com esse nome.")
 
+while True:
     print("\n1 - Adicionar aluno")
     print("2 - Listar alunos")
-    print("3 - Sair")
+    print("3 - Buscar aluno")
+    print("4 - Sair")
 
     opcao = input("Escolha: ")
 
@@ -26,6 +39,10 @@ while True:
         listar_alunos()
 
     elif opcao == "3":
+        buscar_aluno()
+
+    elif opcao == "4":
+        print("Encerrando o programa...")
         break
 
     else:
