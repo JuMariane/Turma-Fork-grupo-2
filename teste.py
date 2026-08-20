98b437c51dca74989fab8e35c467d1bdaa5bf128
Kauan De Palma Cordeiro <linkdomeuvideo@gmail.com>
2026-08-20 18:33:31 -0300

    Excluir pessoas

--- teste.py
+++ teste.py
@@ -11,21 +11,31 @@ def listar_alunos():
     for aluno in alunos:
         print(aluno)
 
+def exclusao():
+    nome = input("Digite o nome do aluno: ")
+    alunos.remove(nome)
+    print("Aluno removido com sucesso!")
+
+
 while True:
 
     print("\n1 - Adicionar aluno")
     print("2 - Listar alunos")
-    print("3 - Sair")
-
+    print("3 - Excluir alunos")
+    print("4 - Sair")
     opcao = input("Escolha: ")
 
     if opcao == "1":
         adicionar_aluno()
 
+
+
     elif opcao == "2":
         listar_alunos()
 
     elif opcao == "3":
+        exclusao()
+    elif opcao == "4":
         break
 
     else:
